29861628aeddd4b57518fb6eeff87ce9cf6c7fea
unknown <blanco-create>
2025-06-25 18:32:00 +0000

    Projet UrubusGPT prêt et fonctionnel

--- app/src/main/java/com/urubusgpt/app/MainActivity.kt
+++ app/src/main/java/com/urubusgpt/app/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.urubusgpt.app
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.urubusgpt.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
